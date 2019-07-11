1f26a631420b53556fda23e8b80d279384cb7b7f
Jude DSouza <jude.dsouza@magine.com>
2019-07-11 15:51:00 +0200

    Include httpstream module

--- modules.go
+++ modules.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "github.com/dsouzajude/logspout-fluentd/fluentd"
+	_ "github.com/gliderlabs/logspout/httpstream"
 	_ "github.com/gliderlabs/logspout/transports/tcp"
 	_ "github.com/gliderlabs/logspout/transports/udp"
 )
