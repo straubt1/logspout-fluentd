e69f4adff791086c71c0022ee6270016fc1c8ced
Tom Straub <tstraub@hashicorp.com>
2020-07-16 18:02:30 -0500

    Update for Audit Logs

--- fluentd/adapter.go
+++ fluentd/adapter.go
@@ -84,6 +84,14 @@ func (ad *Adapter) Stream(logstream chan *router.Message) {
 			"container_name": message.Container.Name,
 			"source":         message.Source,
 		}
+		
+		// Check if Audit Log
+               	if strings.Contains(message.Data, "[Audit Log]") {
+                	start := strings.Index(message.Data, "{")
+                        record["auditlog"] = message.Data[start:]
+			record["source"] = "Audit Log"
+                }
+		
 		log.Println(tag, message.Time, record)
 
 		// Send to fluentd
